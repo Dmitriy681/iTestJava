0553a2badab89399409244f94a382f7af44c229a
Dmitriy681 <132404448+Dmitriy681@users.noreply.github.com>
2023-12-15 22:21:46 +0300

    Первый урок JAVA

---
+++ src/LearnJava111/Main.java
@@ -0,0 +1,17 @@
+package LearnJava111;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print(" World");
+        System.out.println("!");
+        System.out.print("Testing!");
+        System.out.print("\nVolkswagen\nJetta 6");
+        // учу JAVA =)
+        /*
+        комментарий версия 2.0
+         */
+    }
+
+}
